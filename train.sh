1f6f19d64bedae93955cf7620ef0fb6dec1538fe
Eric Lee <levanhong05@gmail.com>
2017-07-20 22:18:24 +0700

    Update shell bash

--- train.sh
+++ train.sh
@@ -4,31 +4,35 @@
 # https://github.com/moses-smt/mosesdecoder/tree/master/scripts
 mose_git=https://raw.githubusercontent.com/moses-smt/mosesdecoder/master/scripts
 
-echo 'select the source language: en, cs, fi, fr, ru, de'
+echo 'Select the source language: en, cs, fi, fr, ru, de'
 read -p '==> ' source_language
-echo "the selected source language is $source_language"
+echo "The selected source language is $source_language"
 
 
-echo 'select the target language: en, cs, fi, fr, ru, de'
+echo 'Select the target language: en, cs, fi, fr, ru, de'
 read -p '==> ' target_language
-echo "the selected target language is $target_language"
+echo "The selected target language is $target_language"
 
-if [ $target_language = $source_language ]; then
-    echo 'languages should be different'
+if [ $target_language = $source_language ]
+then
+    echo 'Languages should be different'
+    read -p ' ' aaa
     exit -1
 fi
 
-if [ ! -d share/nonbreaking_prefixes ]; then
+if [ ! -d 'share/nonbreaking_prefixes' ]
+then
     mkdir -p share/nonbreaking_prefixes 
 fi
 
-echo "downloading nonbreaking_prefix $source_language ..."
+echo "Downloading nonbreaking_prefix $source_language ..."
 curl -s $mose_git/share/nonbreaking_prefixes/nonbreaking_prefix.$source_language > share/nonbreaking_prefixes/nonbreaking_prefix.$source_language
-echo "downloading nonbreaking_prefix $target_language ..."
+echo "Downloading nonbreaking_prefix $target_language ..."
 curl -s $mose_git/share/nonbreaking_prefixes/nonbreaking_prefix.$target_language > share/nonbreaking_prefixes/nonbreaking_prefix.$target_language
 
-if [ ! -d data ]; then
-    echo 'creating data directory...'
+if [ ! -d 'data' ]
+then
+    echo 'Creating data directory...'
     mkdir data
 fi
 
@@ -38,29 +42,30 @@ cp preprocess/create_vocab.py preprocess/shuffle_data.py data/
 
 echo 'cd to data directory'
 cd data
-if [ ! -f tokenizer.perl ]; then
-    echo 'downloading tokenizer.perl'
+if [ ! -f tokenizer.perl ]
+then
+    echo 'Downloading tokenizer.perl'
     curl -s $mose_git/tokenizer/tokenizer.perl > tokenizer.perl
 fi
-if [ ! -f multi-bleu.perl ]; then
-    echo 'downloading multi-bleu.perl'
+if [ ! -f multi-bleu.perl ]
+then
+    echo 'Downloading multi-bleu.perl'
     curl -s $mose_git/generic/multi-bleu.perl > multi-bleu.perl
 fi
 
-echo 'please download corresponding datasets from WMT15 manually'
-echo 'and put the parallel corpus in the data directory'
-echo 'then enter the file name of source language dataset'
+echo 'Please download corresponding datasets from WMT15 manually and put the parallel corpus in the data directory'
+echo 'Then enter the file name of source language dataset'
 read -p '==> ' source_data
-echo 'the file name of target language dataset'
+echo 'The file name of target language dataset'
 read -p '==> ' target_data
 
 if [ ! -f $source_data ]; then
-    echo 'no such source dataset file'
+    echo 'No such source dataset file'
     exit -1
 fi
 
 if [ ! -f $target_data ]; then
-    echo 'no such target dataset file'
+    echo 'No such target dataset file'
     exit -1
 fi
 
@@ -70,19 +75,19 @@ tok_target_file=all.$source_language-$target_language.$target_language.tok
 perl tokenizer.perl -l $source_language -threads 4 -no-escape < $source_data > $tok_source_file
 perl tokenizer.perl -l $target_language -threads 4 -no-escape < $target_data > $tok_target_file
 
-echo 'please put test dataset in the data directory'
-echo 'the file name of source test set:'
+echo 'Please put test dataset in the data directory'
+echo 'The file name of source test set:'
 read -p '==>' src_test
-echo 'the file name of targe test set:'
+echo 'The file name of targe test set:'
 read -p '==>' trg_test
 
 if [ ! -f $src_test ]; then
-    echo 'no such source test file'
+    echo 'No such source test file'
     exit -1
 fi
 
 if [ ! -f $trg_test ]; then
-    echo 'no such target test file'
+    echo 'No such target test file'
     exit -1
 fi
 
@@ -90,12 +95,12 @@ perl tokenizer.perl -l $source_language -threads 4 -no-escape < $src_test > $src
 perl tokenizer.perl -l $target_language -threads 4 -no-escape < $trg_test > $trg_test.tok
 
 
-echo 'please ensure there is enough disk space'
+echo 'Please ensure there is enough disk space'
 python shuffle_data.py $tok_source_file $tok_target_file
 
-echo 'please enter the size of source language vocabulary (120 is enough):'
+echo 'Please enter the size of source language vocabulary (120 is enough):'
 read -p '==>' src_vocab_size
-echo 'please enter the size of target language vocabulary (120 is enough):'
+echo 'Please enter the size of target language vocabulary (120 is enough):'
 read -p '==>' trg_vocab_size
 python create_vocab.py $source_language $target_language $src_vocab_size $trg_vocab_size $tok_source_file.shuf $tok_target_file.shuf
 
@@ -113,8 +118,5 @@ sed -i "s/--trg_vocab_size--/$trg_vocab_size/" configurations.py
 sed -i "s/--src_test--/$src_test/" configurations.py
 sed -i "s/--trg_test--/$trg_test/" configurations.py
 
-echo "ok! just run 'python training_adam.py'"
-
-
-
+echo "OK! Just run 'python training_adam.py'"
 
